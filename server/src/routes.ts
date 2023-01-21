372693ebb49727c2d7993c486873e07152ddcbc7
Antonio <toninetocontato@gmail.com>
2023-01-21 01:37:46 -0300

    create and organize routes

---
+++ server/src/routes.ts
@@ -0,0 +1,35 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from "./lib/prisma"
+import { z } from 'zod'
+
+export async function appRoutes(app: FastifyInstance) {
+    app.post('/habits', async (request) => {
+        /**
+         * Rota de criação de hábitos, recebe
+         * title e weekDays (recorrência do hábito)
+         */
+        const createHabitBody = z.object({
+            tittle: z.string(),
+            weekDays: z.array(z.number().min(0).max(6))
+        })
+        
+        const { tittle, weekDays } = createHabitBody.parse(request.body)
+        
+        const today = dayjs().startOf('day').toDate()
+
+        await prisma.habit.create({
+            data: {
+                tittle,
+                created_at: today,
+                weekDays: {
+                    create: weekDays.map(weekDay => {
+                        return {
+                            week_day: weekDay,
+                        }
+                    })
+                }
+            }
+        })
+
+    })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,11 +6,10 @@
  */
 
 import Fastify from 'fastify'
-import {PrismaClient} from '@prisma/client'
 import cors from '@fastify/cors'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 /** HTTP
  * Get (busca info), 
@@ -22,12 +21,7 @@ const prisma = new PrismaClient()
  */
 
 app.register(cors)
-
-app.get('/hello', async () => {
-    const habits = await prisma.habit.findMany()
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
