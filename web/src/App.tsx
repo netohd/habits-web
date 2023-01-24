105b7118ffde1c33263e370d2bc2b0baae791f5d
Antonio <toninetocontato@gmail.com>
2023-01-24 03:02:31 -0300

    create boxes component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,17 @@
 // import { Habit } from './components/Habit'
 import './styles/global.css'
-import logoImage from './assets/logo.svg'
-
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
 export function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full max-w-5xl px-6 flex-col gap-16'>
-        <img src={logoImage} alt="Habits" />
+
+        <Header />
+        <SummaryTable />
+
       </div>
-      
     </div>
   )
 }
