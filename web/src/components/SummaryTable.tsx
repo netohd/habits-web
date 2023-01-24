10399ad959f7e3c5961a1d9ace9084c1fd65d8b2
Antonio <toninetocontato@gmail.com>
2023-01-24 03:35:54 -0300

    index fix

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -27,7 +27,7 @@ export function SummaryTable() {
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
           return (
-            <div key={} className="bg-zinc-900 w-10 h-10 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+            <div key={i} className="bg-zinc-900 w-10 h-10 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
           )
           })}
       </div>
