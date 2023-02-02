70893a27c1b44f48b07e69e4667eeb1b480283ee
Antonio <toninetocontato@gmail.com>
2023-02-01 23:58:38 -0300

    key comment

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -12,6 +12,7 @@ export function SummaryTable() {
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {/* Índice é usado como key */}
         {weekDays.map((weekDay, i) => {
           return (
             <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
