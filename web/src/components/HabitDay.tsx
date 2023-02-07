82864fc3222b6c7c827e2280975036276c398e13
Antonio <toninetocontato@gmail.com>
2023-02-07 02:06:39 -0300

    form and habitday adds

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,7 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
 import * as Popover from '@radix-ui/react-popover';
 import clsx from 'clsx';
+import { Check } from 'phosphor-react';
 import { ProgressBar } from './ProgressBar'
 
 interface HabitDayProps {
@@ -7,18 +9,21 @@ interface HabitDayProps {
   amount: number
 }
 
+/** Modal que aparece quando se clica em algum dos quadros do sumário (dias)
+ * Utilização do radix (lib) para estilização e recurso de acessibilidade
+ */
 export function HabitDay(props: HabitDayProps) {
-  const completedPercentage = Math.round((props.completed/props.amount) * 100)
+  const completedPercentage = Math.round((props.completed / props.amount) * 100)
 
   return (
     <Popover.Root>
       {
-      /** clsx é utilizado para passar atributos css e criar condicionais
-       * para cor de acordo com porcentagem
-       * ==> npm install clsx
-       */  
+        /** clsx é utilizado para passar atributos css e criar condicionais
+         * para cor de acordo com porcentagem
+         * ==> npm install clsx
+         */
       }
-      <Popover.Trigger 
+      <Popover.Trigger
         className={clsx('bg-zinc-900 w-10 h-10 border-2 border-zinc-800 rounded-lg', {
           'bg-zinc-900 border-zinc-800': completedPercentage === 0,
           'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
@@ -26,8 +31,8 @@ export function HabitDay(props: HabitDayProps) {
           'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
           'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
           'bg-violet-500 border-violet-400': completedPercentage >= 80,
-        })} 
-    />
+        })}
+      />
 
       <Popover.Portal>
         <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
@@ -35,6 +40,19 @@ export function HabitDay(props: HabitDayProps) {
           <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
 
           <ProgressBar progress={completedPercentage} />
+
+          <div className='mt-6 flex flex-col gap-3'>
+            <Checkbox.Root className='flex items-center gap-3 group'>
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                <Checkbox.Indicator>
+                  <Check  size={20} className='text-white' />
+                </Checkbox.Indicator>
+              </div>
+              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+                Beber 2L de agua
+              </span>
+            </Checkbox.Root>
+          </div>
 
           <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
         </Popover.Content>
