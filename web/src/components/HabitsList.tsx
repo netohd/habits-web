456670929cf9aced460054b95d0cf36a27c631db
Antonio <toninetocontato@gmail.com>
2023-02-09 20:47:14 -0300

    HabitList checkbox key and checked add

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -1,35 +1,57 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { api } from '../lib/axios';
 
 interface HabitListProps {
   date: Date
 }
 
-export function HabitsList({ date }: HabitListProps ) {
+interface HabitsInfo {
+  possibleHabits: {
+    id: string;
+    title: string;
+    created_at: string;
+  }[],
+  completedHabits: string[]
+}
+
+export function HabitsList({ date }: HabitListProps) {
+  const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
+
+
+  // Macete para executar chamada somente uma vez (useEffect)
   useEffect(() => {
     api.get('day', {
       params: {
         date: date.toISOString(),
       }
     }).then(response => {
-      console.log(response.data)
+      setHabitsInfo(response.data)
     })
   }, [])
-  
+
   return (
-          <div className='mt-6 flex flex-col gap-3'>
-            <Checkbox.Root className='flex items-center gap-3 group'>
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
-                <Checkbox.Indicator>
-                  <Check size={20} className='text-white' />
-                </Checkbox.Indicator>
-              </div>
-              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
-                Beber 2L de agua
-              </span>
-            </Checkbox.Root>
-          </div>
+    <div className='mt-6 flex flex-col gap-3'>
+      {habitsInfo?.possibleHabits.map(habit => {
+        return (
+          // Primeiro elemento que vem no map precisa da key
+          <Checkbox.Root 
+            key={habit.id} 
+            checked={habitsInfo.completedHabits.includes(habit.id)} 
+            className='flex items-center gap-3 group'
+          >
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+              <Checkbox.Indicator>
+                <Check size={20} className='text-white' />
+              </Checkbox.Indicator>
+            </div>
+            <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+              {habit.title}
+            </span>
+          </Checkbox.Root>
+        )
+      })}
+    </div>
   )
 }
