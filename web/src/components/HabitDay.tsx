991f67f0fa7cd90f5fed2be01d806c5529f393c0
Antonio <toninetocontato@gmail.com>
2023-02-10 10:41:52 -0300

    transitions and focus style add

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -35,7 +35,7 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
          */
       }
       <Popover.Trigger
-        className={clsx('bg-zinc-900 w-10 h-10 border-2 border-zinc-800 rounded-lg', {
+        className={clsx('bg-zinc-900 w-10 h-10 border-2 border-zinc-800 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background', {
           'bg-zinc-900 border-zinc-800': completedPercentage === 0,
           'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
           'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -50,7 +50,7 @@ export function NewHabitForm() {
       <input type="text"
         id="title"
         placeholder="ex.: Exercicios, dormir mais, etc.."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900"
         autoFocus 
         value={title}
         // Toda vez que é digitado o valor no input vai anotar na var Title
@@ -66,11 +66,11 @@ export function NewHabitForm() {
           // Quando map é usado, primeiro elemento precisa ter key com valores únicos
           <Checkbox.Root
             key={weekDay}
-            className='flex items-center gap-3 group'
+            className='flex items-center gap-3 group focus:outline-none'
             checked={weekDays.includes(index)}
             onCheckedChange={() => handleToggleWeekDay(index)}
           >
-            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background'>
               <Checkbox.Indicator>
                 <Check size={20} className='text-white' />
               </Checkbox.Indicator>
@@ -84,7 +84,7 @@ export function NewHabitForm() {
 
       </div>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
         <Check size={20} weight="bold" />
         Confirmar
       </button>
